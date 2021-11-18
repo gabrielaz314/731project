94304103dd1e2908c142e926d753a07b29c6689e
Chris Jiang <Christopherjiang@outlook.com>
2021-11-18 14:15:15 -0500

    nodemon to watch for hbs changes as well view-all-courses added menu active link responds to windowpath fulfilled-req started

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const calendar = require('./testdata').calendar;
 
-
+/* Homepage */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Home' });
+});
+/* Application cannot be accessed until a user has logged in */
 router.get('/login', function(req, res) {
   res.render('login', { title: 'Login Page' });
 });
 
-/* Homepage directs to login */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Home' });
+/* App Routing */
+
+// Helper function to render semesters in 2 columns
+function splitSemesters(calendar) {
+  var a = [];
+  var b = [];
+
+  calendar.forEach((semester, index) => {
+    if (index % 2 == 0)
+      a.push(semester)
+    else
+      b.push(semester)
+  });
+  return [a,b]
+}
+router.get('/view-all-courses', function(req, res) {
+  res.render('view-all-courses', {
+    title: 'View All Courses',
+    layout: '/layouts/sb-content',
+    calendar: splitSemesters(calendar),
+  });
+});
+
+router.get('/fulfilled-requirements', function(req, res) {
+  res.render('fulfilled-requirements', {
+    title: 'Fulfilled Requirements',
+    layout: '/layouts/sb-content',
+  });
 });
 
 module.exports = router;
---
+++ routes/testdata.js
@@ -0,0 +1,82 @@
+const calendar = [
+  {
+    name: "1a",
+    courses:[
+      {
+        code: "CPS731",
+        name: "Software Engineering I",
+        description: "Essential methods for the Systematic approach to the development, operation, maintenance, and retirement of Software. Software Life Cycles, process modelling, configuration management, managing Software",
+        prerequisites: "CPS406",
+        corequisites: "none",
+        antirequisites: "none",
+      },
+      {
+        code: "CPS731",
+        name: "Software Engineering I",
+        description: "Essential methods for the Systematic approach to the development, operation, maintenance, and retirement of Software. Software Life Cycles, process modelling, configuration management, managing Software",
+        prerequisites: "CPS406",
+        corequisites: "none",
+        antirequisites: "none",
+      },
+      {
+        code: "CPS731",
+        name: "Software Engineering I",
+        description: "Essential methods for the Systematic approach to the development, operation, maintenance, and retirement of Software. Software Life Cycles, process modelling, configuration management, managing Software",
+        prerequisites: "CPS406",
+        corequisites: "none",
+        antirequisites: "none",
+      },
+    ]
+  },
+  {
+    name: "1b",
+    courses:[
+      {
+        code: "CPS731",
+        name: "Software Engineering I",
+        description: "Essential methods for the Systematic approach to the development, operation, maintenance, and retirement of Software. Software Life Cycles, process modelling, configuration management, managing Software",
+        prerequisites: "CPS406",
+        corequisites: "none",
+        antirequisites: "none",
+      },
+      {
+        code: "CPS731",
+        name: "Software Engineering I",
+        description: "Essential methods for the Systematic approach to the development, operation, maintenance, and retirement of Software. Software Life Cycles, process modelling, configuration management, managing Software",
+        prerequisites: "CPS406",
+        corequisites: "none",
+        antirequisites: "none",
+      },
+    ]
+  },
+  {
+    name: "2a",
+    courses:[
+      {
+        code: "CPS731",
+        name: "Software Engineering I",
+        description: "Essential methods for the Systematic approach to the development, operation, maintenance, and retirement of Software. Software Life Cycles, process modelling, configuration management, managing Software",
+        prerequisites: "CPS406",
+        corequisites: "none",
+        antirequisites: "none",
+      },
+    ]
+  },
+  {
+    name: "2b",
+    courses:[]
+  },
+  {
+    name: "3a",
+    courses:[]
+  },
+  {
+    name: "3b",
+    courses:[]
+  },
+]
+
+
+module.exports = {
+  calendar: calendar,
+};
