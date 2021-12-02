9d8a33a441499e0958c0b45917e725779663de39
SauceOu <79672698+SauceOu@users.noreply.github.com>
2021-12-02 18:20:48 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -59,5 +59,21 @@ router.get('/non-related', function(req, res) {
     non_related_courses: non_related_courses,
   });
 });
+router.get('/view-saved-courses', function(req, res) {
+  res.render('view-saved-courses', {
+    title: 'View Saved Courses',
+    layout: '/layouts/sb-content',
+    // using the dummy data for transfer_courses
+    saved_courses: transfer_courses,
+  });
+});
+router.get('/view-course-plan', function(req, res) {
+  res.render('view-course-plan', {
+    title: 'View Courses Plan',
+    layout: '/layouts/sb-content',
+    // using the dummy data for calendar
+    course_plan: splitSemesters(calendar),
+  });
+});
 
 module.exports = router;
